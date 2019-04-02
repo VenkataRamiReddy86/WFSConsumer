be5f65301402967f3b06bd939a464b24733bbfcc
rv <venkataramireddy86@gmail.com>
2019-04-03 01:38:35 +0530

    Add project files.

---
+++ WFSConsumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+
+namespace WFSConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var url = "https://services.nationalmap.gov/arcgis/services/WFS/Contours/MapServer/WFSServer?request=getcapabilities&service=wfs";
+            HttpClient client = new HttpClient();
+
+          var response=  client.GetAsync(url);
+            response.Wait();
+          var result= response.Result.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
